9ffed20e6b4c17907bd6c4598c9364662ff9cdc5
Nicodemos Santos <saviogomes_182@hotmail.com>
2022-04-27 09:54:05 +0100

    Finished swagger implementation

--- resources/views/swagger/index.blade.php
+++ resources/views/swagger/index.blade.php
@@ -1,5 +1,8 @@
 <html>
 <head>
+    <meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate"/>
+    <meta http-equiv="Pragma" content="no-cache"/>
+    <meta http-equiv="Expires" content="0"/>
     <title>{{ config('app.name') }} | Frontend API's Swagger</title>
     <link href="{{asset('swagger/style.css')}}" rel="stylesheet">
 </head>
